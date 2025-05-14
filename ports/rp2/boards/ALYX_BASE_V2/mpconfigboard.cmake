4c2b7592f5a85d7910502764d4cfc706081744e3
Alex <130048160+bauer-alex-174@users.noreply.github.com>
2025-05-14 13:32:42 +0200

    rp2: add alyx_base_v2
    Signed-off-by: Alex <bauer.alex.174@gmail.com>

---
+++ ports/rp2/boards/ALYX_BASE_V2/mpconfigboard.cmake
@@ -0,0 +1,3 @@
+# cmake file for Raspberry Pi Pico
+set(PICO_BOARD "alyx_base_v2")
+set(PICO_PLATFORM "rp2040")
---
+++ ports/rp2/boards/ALYX_BASE_V2/mpconfigboard.h
@@ -0,0 +1,3 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Alyx Base V3"
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (2 * 1024 * 1024)
